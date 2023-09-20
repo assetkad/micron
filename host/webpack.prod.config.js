95f9562b13b34eb682439e14822fe2d98f5b25a8
Asset Kad <asset_kad@mail.ru>
2023-09-20 16:24:33 +0500

    загружаю на хост

--- host/webpack.prod.config.js
+++ host/webpack.prod.config.js
@@ -2,14 +2,9 @@ const { withModuleFederation } = require('@nx/angular/module-federation');
 const config = require('./module-federation.config');
 module.exports = withModuleFederation({
   ...config,
-  /*
-   * Remote overrides for production.
-   * Each entry is a pair of a unique name and the URL where it is deployed.
-   *
-   * e.g.
-   * remotes: [
-   *   ['app1', 'https://app1.example.com'],
-   *   ['app2', 'https://app2.example.com'],
-   * ]
-   */
+
+  remotes: [
+    ['todo', 'https://heroic-medovik-a142f3.netlify.app'],
+    ['login', 'https://stately-gecko-3024da.netlify.app/'],
+  ],
 });
